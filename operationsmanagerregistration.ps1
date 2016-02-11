813df5f8b533df5633913e621e5d1349bfcdd835
Rob Nelson <rnelson0@gmail.com>
2016-02-11 17:00:24 -0500

    New cmdlet New-OperationsManagerRegistrationRole for use with vROps Manager.

---
+++ operationsmanagerregistration.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.Synopsis
+   Create a vCenter role for vRealize Operations Manager Registration
+.DESCRIPTION
+   Create vCenter role for vRealize Operations Manager Registration using the minimum permissions required
+.EXAMPLE
+   New-OperationsManagerRole
+.EXAMPLE
+   New-OperationsManagerRole 'vROps-Reg'
+.INPUTS
+   $OperationsManager_Role (string)
+#>
+function New-OperationsManagerRegistrationRole
+{
+    Param
+    (
+        # The name of the role for the vCenter Operations Manager Registration user
+        [String]
+        $OperationsManagerRegistration_Role = 'vROps-Registration'
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $PrivilegeList = Get-VIPrivilege -Id @(
+            'Extension.Register'
+            'Extension.Unregister'
+            'Extension.Update'
+            'Global.Licenses'
+        )
+        New-VIRole $OperationsManagerRegistration_Role
+        Set-VIRole $OperationsManagerRegistration_Role -AddPrivilege $PrivilegeList                                                      
+
+        Get-VIRole $OperationsManagerRegistration_Role | Select Name, PrivilegeList
+    }
+    End
+    {
+    }
+}
