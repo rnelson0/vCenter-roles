ea6843fb4e188747fa308fb31116fa481dee4d43
Rob Nelson <rnelson0@gmail.com>
2016-02-10 20:36:40 -0500

    Add loginsight.ps1 and New-LogInsightRole

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vCenter-roles
 Set up roles for common applications to access vCenter
+
+* `New-LogInsightRole`: Creates a new role with the minimum permissions. Accepts a string for the name of the role. Defaults to `LogInsight-Access`.
---
+++ loginsight.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.Synopsis
+   Create a vCenter role for Log Insight
+.DESCRIPTION
+   Create a vCenter role for Log Insight using the minimum permissions required
+.EXAMPLE
+   New-LogInsightRole
+.EXAMPLE
+   New-LogInsightRole 'LI-Access'
+.INPUTS
+   $LogInsight_Role (string)
+#>
+function New-LogInsightRole
+{
+    Param
+    (
+        # The name of the role for the Log Insight user
+        [String]
+        $LogInsight_Role = 'LogInsight-Access'
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        New-VIRole $LogInsight_Role
+        Set-VIRole $LogInsight_Role -AddPrivilege (Get-VIPrivilege -id  Host.Config.AdvancedConfig)
+        Set-VIRole $LogInsight_Role -AddPrivilege (Get-VIPrivilege -id  Host.Config.NetService)
+        Set-VIRole $LogInsight_Role -AddPrivilege (Get-VIPrivilege -id  Host.Config.Network)
+        Set-VIRole $LogInsight_Role -AddPrivilege (Get-VIPrivilege -id  Host.Config.Settings)
+        Set-VIRole $LogInsight_Role -AddPrivilege (Get-VIPrivilege -id  System.Anonymous)
+        Set-VIRole $LogInsight_Role -AddPrivilege (Get-VIPrivilege -id  System.Read)
+        Set-VIRole $LogInsight_Role -AddPrivilege (Get-VIPrivilege -id  System.View)
+
+        Get-VIRole $LogInsight_Role | Select Name,PrivilegeList
+    }
+    End
+    {
+    }
+}
