1160eff3c8150d2315ffc31161513a250363dabd
Rob Nelson <rnelson0@gmail.com>
2016-02-24 07:52:18 -0500

    Cleanup post-PR10   Adjust formatting: indentation, capitalization, warning and error messages.   Change -Permission to -PermissionsFile, with alias of -Permission; adjust help to match.   Refactor some of the PR10-related code that was not working properly.

--- Import-VIRole.ps1
+++ Import-VIRole.ps1
@@ -2,25 +2,25 @@
 function Import-VIRole
 {
     <#  
-            .SYNOPSIS
-            Imports a vSphere role based on pre-defined configuration values
-            .DESCRIPTION
-            The Import-VIRole cmdlet is used to parse through a list of pre-defined permissions to create a new role. Often, this is to support a particular vendor's set of requirements for access into vSphere.
-            .PARAMETER Name
-            Name of the role. Only alpha and space characters are allowed.
-            .PARAMETER Permission
-            Path to the JSON file containing permissions
-            .PARAMETER vCenter
-            vCenter Server IP or FQDN
-            .EXAMPLE
-            Import-VIRole -Name Banana -Permission "C:\Banana.json" -vCenter VC1.FQDN
-            Creates a new role named Banana, using the permission list stored in Banana.json, and applies it to the VC1.FQDN vCenter Server
-            .NOTES
-            Written by Chris Wahl for community usage
-            Twitter: @ChrisWahl
-            GitHub: chriswahl
-            .LINK
-            https://github.com/chriswahl
+        .SYNOPSIS
+        Imports a vSphere role based on pre-defined configuration values
+        .DESCRIPTION
+        The Import-VIRole cmdlet is used to parse through a list of pre-defined permissions to create a new role. Often, this is to support a particular vendor's set of requirements for access into vSphere.
+        .PARAMETER Name
+        Name of the role. Only alpha and space characters are allowed.
+        .PARAMETER Permission
+        Path to the JSON file containing permissions
+        .PARAMETER vCenter
+        vCenter Server IP or FQDN
+        .EXAMPLE
+        Import-VIRole -Name Banana -PermissionsFile "C:\Banana.json" -vCenter VC1.FQDN
+        Creates a new role named Banana, using the permission list stored in Banana.json, and applies it to the VC1.FQDN vCenter Server
+        .NOTES
+        Written by Chris Wahl for community usage
+        Twitter: @ChrisWahl
+        GitHub: chriswahl
+        .LINK
+        https://github.com/rnelson0/vCenter-roles/
     #>
 
     [CmdletBinding()]
@@ -31,25 +31,23 @@ function Import-VIRole
         [String]$Name,
         [Parameter(Mandatory = $true,Position = 1,HelpMessage = 'Path to the JSON file containing permissions')]
         [ValidateNotNullorEmpty()]
-        [String]$Permission,
+        [Alias("Permission")]
+        [String]$PermissionsFile,
         [Parameter(Mandatory = $true,Position = 2,HelpMessage = 'vCenter Server IP or FQDN')]
         [ValidateNotNullorEmpty()]
         [String]$vCenter,
-        [Parameter(Mandatory = $true,Position = 3,HelpMessage = 'Overwrites existing Role by same name')]
-        [ValidateNotNullorEmpty()]
-        [Boolean]$Overwrite=$false
+        [Parameter(Position = 3,HelpMessage = 'Overwrites existing Role by same name')]
+        [Switch]$Overwrite=$false
     )
 
     Process {
 
-        Write-Verbose -Message 'Importing required modules and snapins'
+        Write-Verbose -Message 'Importing PowerCLI modules and snapins'
         $powercli = Get-PSSnapin -Name VMware.VimAutomation.Core -Registered
-        try 
+        Try 
         {
-            switch ($powercli.Version.Major) {
-                {
-                    $_ -ge 6
-                }
+            Switch ($powercli.Version.Major) {
+                { $_ -ge 6 }
                 {
                     Import-Module -Name VMware.VimAutomation.Core -ErrorAction Stop
                     Write-Verbose -Message 'PowerCLI 6+ module imported'
@@ -61,78 +59,79 @@ function Import-VIRole
                 }
                 default 
                 {
-                    throw 'This script requires PowerCLI version 5 or later'
+                    Throw 'This script requires PowerCLI version 5 or later'
                 }
             }
         }
-        catch 
+        Catch 
         {
-            throw $_
+            Throw $_
         }
 
-        
         Write-Verbose -Message 'Allowing untrusted SSL certs'
         Add-Type -TypeDefinition @"
-	    using System.Net;
-	    using System.Security.Cryptography.X509Certificates;
-	    public class TrustAllCertsPolicy : ICertificatePolicy {
-	        public bool CheckValidationResult(
-	            ServicePoint srvPoint, X509Certificate certificate,
-	            WebRequest request, int certificateProblem) {
-	            return true;
-	        }
-	    }
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,
+                WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
 "@
         [System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
 
-        Write-Verbose -Message 'Ignoring self-signed SSL certificates for vCenter Server (optional)'
+        Write-Verbose -Message "Ignoring self-signed SSL certificates for vCenter server '$vCenter' (optional)"
         $null = Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -DisplayDeprecationWarnings:$false -Scope User -Confirm:$false
 
-        Write-Verbose -Message 'Connecting to vCenter'
-        try 
+        Write-Verbose -Message "Connecting to vCenter server '$vCenter'"
+        Try 
         {
             $null = Connect-VIServer -Server $vCenter -ErrorAction Stop -Session ($global:DefaultVIServers | Where-Object -FilterScript {
                     $_.name -eq $vCenter
             }).sessionId
         }
-        catch 
+        Catch 
         {
-            throw 'Could not connect to vCenter'
+            Throw "Could not connect to vCenter server '$vCenter'"
         }
 
-        Write-Verbose -Message 'Check to see if the role exists'
-        if (Get-VIRole -Name $Name -ErrorAction SilentlyContinue -and $Overwrite -eq $False) 
+        Write-Verbose -Message "Check to see if role '$Name' exists"
+        $RoleExists = Get-VIRole -Name $Name -Server $vCenter -ErrorAction SilentlyContinue
+        if ($RoleExists -And (! $Overwrite)) 
         {
-            throw 'Role already exists'
+            Throw 'Role already exists.'
         }
-	
-	Write-Verbose -Message 'Read the JSON file'
-        $null = Test-Path $Permission
-        [array]$PermArray = Get-Content -Path $Permission -Raw | ConvertFrom-Json
+    
+        Write-Verbose -Message "Read the permissions file '$PermissionsFile'"
+        $null = Test-Path $PermissionsFile
+        [array]$PermissionsArray = Get-Content -Path $PermissionsFile -Raw | ConvertFrom-Json
 
-        Write-Verbose -Message 'Parse the permission array for IDs'
-        $PermList = Get-VIPrivilege -Id $PermArray -ErrorVariable MissingPerm -ErrorAction SilentlyContinue
 
-        Write-Verbose -Message 'Checking for missing permissions in vCenter'
-        if ($MissingPerm)
+        Write-Verbose -Message 'Parse the permissions array for IDs'
+        $PermissionsList = Get-VIPrivilege -Id $PermissionsArray -ErrorVariable MissingPerm -ErrorAction SilentlyContinue
+
+        Write-Verbose -Message "Identify any permissions in the list that are not present on vCenter server '$vCenter'"
+        if ($MissingPermissions)
         {
-            foreach ($_ in $MissingPerm)
+            foreach ($MissingPermission in $MissingPermissions)
             {
-                Write-Warning -Message "Permission named $(($_.Exception.Message.Split("'"))[1]) not found"
+                $PermissionID = ($MissingPermission.Exception.Message.Split("'"))[1]
+                Write-Warning -Message "Permission ID '$PermissionID' not found"
             }
         }
-	if (!$Overwrite)
-	{
-        
-        Write-Verbose -Message 'Create the role'
-        New-VIRole -Name $Name | Set-VIRole -AddPrivilege $PermList
-	}
-	if (Get-VIRole -Name $Name -ErrorAction SilentlyContinue -and $OverWrite) 
-	{
-	
-	Write-Verbose -Message 'Overwriting role'
-	Get-VIRole -Name $Name | Set-VIRole -RemovePrivilege (Get-VIPrivilege $_)
-	Get-VIRoleSet-VIRole -AddPrivilege $PermList
-	}
-    } # End of process
-} # End of function
+
+        if ((! $RoleExists) -Or !$Overwrite)
+        {
+            Write-Verbose -Message "Create the role '$Name'"
+            New-VIRole -Name $Name | Set-VIRole -AddPrivilege $PermissionsList
+        }
+        elseif ($RoleExists -And ($OverWrite)) 
+        {
+            Write-Verbose -Message "Overwrite the role '$Name'"
+            Get-VIRole -Name $Name | Set-VIRole -RemovePrivilege *
+            Get-VIRole -Name $Name | Set-VIRole -AddPrivilege $PermissionsList
+        }
+    }
+}
